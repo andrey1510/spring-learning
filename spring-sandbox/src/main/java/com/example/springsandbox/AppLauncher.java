81d0c2c95b97fdc2f01c09bcc10dcf7166ee8958
andrey1510 <andrey-1510@outlook.com>
2023-08-04 14:37:46 +0300

    minor update

--- spring-sandbox/src/main/java/com/example/springsandbox/AppLauncher.java
+++ spring-sandbox/src/main/java/com/example/springsandbox/AppLauncher.java
@@ -5,6 +5,9 @@ import com.example.springsandbox.clients.Client;
 import com.example.springsandbox.clients.Vendor;
 import com.example.springsandbox.configs.AppConfig;
 import com.example.springsandbox.services.VendorService;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppLauncher {
@@ -14,6 +17,8 @@ public class AppLauncher {
 
         Vendor vendor = context.getBean(Vendor.class);
         vendor.getData();
+        vendor.printBeanName();
+        vendor.doSomething();
 
         Client client = context.getBean(Client.class);
         client.print();
--- spring-sandbox/src/main/java/com/example/springsandbox/clients/Vendor.java
+++ spring-sandbox/src/main/java/com/example/springsandbox/clients/Vendor.java
@@ -1,13 +1,41 @@
 package com.example.springsandbox.clients;
 
 import com.example.springsandbox.services.VendorService;
-import jakarta.annotation.Resource;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Vendor {
+public class Vendor implements BeanNameAware {
 
-    @Resource(name = "vendorServiceBean1")
+    private static Logger log = LoggerFactory.getLogger(Vendor.class);
+
+    public void doSomething() {
+        log.info("doing something");
+    }
+
+    @Value("${name}:jjj")
+    private String name;
+    
+    private String beanName;
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    public void printBeanName() {
+        System.out.println("Bean Name: " + beanName);
+        System.out.println(name);
+    }
+
+
+
+
+    @Autowired
+    @Qualifier("vendorServiceBean1")
     public VendorService vendorService1;
 
     @Autowired
