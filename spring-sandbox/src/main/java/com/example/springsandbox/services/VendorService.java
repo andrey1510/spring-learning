bc0669a63d2bc6eec62f20b95de96c7c117247de
alkour <alkour@yandex.ru>
2023-08-04 14:42:53 +0300

    Example with ApplicationContextAware

--- spring-sandbox/src/main/java/com/example/springsandbox/services/VendorService.java
+++ spring-sandbox/src/main/java/com/example/springsandbox/services/VendorService.java
@@ -1,10 +1,40 @@
 package com.example.springsandbox.services;
 
 
-public class VendorService {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Arrays;
+
+public class VendorService implements BeanNameAware, ApplicationContextAware {
+    // Конструктор, чтобы поймать инициализациюю
+    private String beanName;
+    private ApplicationContext applicationContext;
 
     public void printVendor() {
-        System.out.println("print vendor");
+        System.out.println("print vendor" + " bean type:" + this.getClass() + " bean name:" + beanName);
+        if (applicationContext.isPrototype(beanName)) {
+            System.out.println("Prototype");
+        } else {
+            System.out.println("Singleton");
+        }
+        printListOfBeans();
     }
 
+    @Override
+    public void setBeanName(String name) {
+        beanName = name;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    private void printListOfBeans() {
+        applicationContext.getBeanDefinitionNames();
+        Arrays.asList(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
+    }
 }
